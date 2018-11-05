f63f58bc104669d69772dc47742755a6559c2b73
Mike McCready <michaeljmccready@gmail.com>
2018-11-04 21:35:53 -0800

    import contract interface and set deployed address, set channel methods

--- src/App.js
+++ src/App.js
@@ -1,148 +1,214 @@
-import React, { Component } from 'react'
-import abi from 'ethereumjs-abi'
-import util from 'ethereumjs-util'
-import './App.css'
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import contractInterface from './interfaces/UnidirectionalPaymentChannelInterface';
+import './App.css';
 
-let web3
+const contractAddress = '0xf9e1c55416d8c9b76a8d550da5d6046c6091deef'
+let web3;
+let channelContract;
 
 class App extends Component {
   state = {
-    channelContractAddress: '',
-    paymentAmount: '',
-    contractBalance: '',
-    verifyMessage: ''
+    openPaymentRecipient: '',
+    openPaymentAmount: '',
+    senderChannels: [],
+    paymentAmountEth: '0.005'
   }
 
   componentDidMount() {
+    this.initWeb3();
+  }
+
+  initWeb3() {
     if (typeof window.web3 !== 'undefined') {
-      window.web3 = new window.Web3(window.web3.currentProvider)
-      web3 = window.web3
+      web3 = new Web3(window.web3.currentProvider);
+      window.web3 = web3;
+      this.loadContractInterface();
     } else {
-      alert('no metamask')
+      alert('no web3')
     }
   }
 
-  fetchInitialChannelAmount(e) {
-    e.preventDefault()
-    const { channelContractAddress } = this.state
-    const contractBalance = web3.eth.getBalance(channelContractAddress, (err, balance) => {
-      alert(balance)
-      localStorage.setItem('contractAddress', channelContractAddress)
-    })
+  loadContractInterface() {
+    channelContract = new web3.eth.Contract(
+      contractInterface,
+      contractAddress
+    );
+    this.getSenderChannels();
   }
 
-  constructPaymentMessage(e) {
-    e.preventDefault()
-    const { channelContractAddress, paymentAmount } = this.state
+  async getSenderChannels() {
+    const from = await web3.eth.getAccounts();
 
-    const message = abi.soliditySHA3(
-      [ 'address', 'uint256' ],
-      [ channelContractAddress, paymentAmount ]
-    )
+    const channelCount = await channelContract.methods
+      .senderChannelIndex(from[0]).call()
 
-    return this.signPaymentMessage(message)
+    if (channelCount < 1) return;
+
+    // fetch channel data from contract
+    const senderChannels = []
+    for (let i = 0; i < channelCount; i++) {
+      let result = await channelContract.methods
+        .senderChannels(from[0], i).call()
+      senderChannels.push(result)
+    }
+
+    this.setState({ senderChannels })
   }
 
-  signPaymentMessage(message) {
-    const component = this
+  async openChannel(e) {
+    e.preventDefault();
+    const { openPaymentRecipient, openPaymentAmount } = this.state;
+    const value = web3.utils.toWei(openPaymentAmount, 'ether');
+    const accounts = await web3.eth.getAccounts();
 
-    web3.personal.sign(
-      '0x' + message.toString('hex'),
-      web3.eth.defaultAccount,
-      (err, signedMessage) => component.setState({ signedMessage })
+    channelContract.methods
+      .openChannel(openPaymentRecipient)
+      .send({ from: accounts[0], value })
+      .then(receipt => this.getSenderChannels())
+  }
+
+  async initPayment(channelId) {
+    const { paymentAmountEth } = this.state;
+    const amount = web3.utils.toWei(paymentAmountEth, 'ether');
+    const accounts = await web3.eth.getAccounts();
+
+    const previousChannelSpend = this.getPreviousChannelSpend(channelId)
+
+    const channelSpend = (parseInt(amount) + parseInt(previousChannelSpend)).toString();
+    const message = this.constructPaymentMessage(channelSpend, channelId);
+    const signature = await this.signPaymentMessage(message);
+
+    const paymentEntry = {
+      channelId,
+      channelSpend,
+      signature,
+      spenderAddress: accounts[0]
+    }
+
+    this.storePayment(paymentEntry)
+  }
+
+  constructPaymentMessage(channelSpend, channelId) {
+    return web3.utils.soliditySha3(
+      { t: 'uint256', v: channelSpend },
+      { t: 'uint256', v: channelId },
     )
   }
 
-  verifyPayment(e) {
-    e.preventDefault()
+  async signPaymentMessage(message) {
+    const accounts = await web3.eth.getAccounts();
+    const signedMessage = await web3.eth.personal.sign(message, accounts[0])
+    return signedMessage;
+  }
 
-    const { channelContractAddress, paymentAmount, signedMessage } = this.state
+  getPreviousChannelSpend(channelId) {
+    if (!localStorage.getItem('payments')) {
+      localStorage.setItem('payments', JSON.stringify({}))
+      return 0;
+    }
 
-    const verifyMessage = abi.soliditySHA3(
-      [ 'string', 'address', 'uint256' ],
-      [ '\x19Ethereum Signed Message:\n', channelContractAddress, paymentAmount ]
-    )
+    const storedPayments = JSON.parse(localStorage.getItem('payments'));
 
-    // const prefix = new Buffer("\x19Ethereum Signed Message:\n");
-    // const prefixedMsg = abi.soliditySHA3(
-    //   Buffer.concat([prefix, new Buffer(String(verifyMessage.length)), verifyMessage])
-    // );
+    const previousSpend = storedPayments[channelId] ?
+      storedPayments[channelId].channelSpend : 0;
 
-    console.log('verifying', '0x' + verifyMessage.toString('hex'), signedMessage)
+    return previousSpend.toString();
+  }
 
+  storePayment(paymentEntry) {
+    // update payment object from storage
+    const paymentStorage = localStorage.getItem('payments') ?
+      Object.assign({}, JSON.parse(localStorage.getItem('payments'))) : {};
 
-    const split = util.fromRpcSig(signedMessage);
-    const publicKey = util.ecrecover(verifyMessage, split.v, split.r, split.s);
-    const signer = util.pubToAddress(publicKey).toString("hex");
+    paymentStorage[paymentEntry.channelId] = paymentEntry;
+    localStorage.setItem('payments', JSON.stringify(paymentStorage))
+  }
 
-    console.log('signer', signer, 'publicKey', publicKey, 'split', split)
-    console.log('expected', util.stripHexPrefix('0xB45fe4e017cf0Ede6350fD0889BD636D92FB5f60'))
-    // return signer;
+  async verifySignature(channelId) {
+    const storedPayment = JSON.parse(localStorage.getItem('payments'))[channelId];
+    const { channelSpend, signature, spenderAddress } = storedPayment;
 
+    const expectedMessage = this.constructPaymentMessage(channelSpend, channelId);
+    const signingAddress = await web3.eth.personal.ecRecover(expectedMessage, signature);
+
+    if (signingAddress.toLowerCase() === spenderAddress.toLowerCase())
+      return alert('payment signature is valid. Signed by: ' + signingAddress + ' for ' + channelSpend);
+
+    alert('payment signature invalid')
   }
 
   render() {
-    const { channelContractAddress, signedMessage } = this.state
+    const {
+      openPaymentRecipient,
+      openPaymentAmount,
+      senderChannels,
+      paymentAmountEth
+    } = this.state;
 
-    return (
-      <div className="App">
+    return(
+      <div>
+        <h1>Open payment channel</h1>
         <form>
           <label>
-            Enter Payment Channel Address
+            Enter Channel Recipient
             <input
               type="text"
-              onChange={e => this.setState({ channelContractAddress: e.target.value })} />
+              placeholder="recipient address 0x.."
+              value={openPaymentRecipient}
+              onChange={e => this.setState({ openPaymentRecipient: e.target.value })} />
           </label>
-          <button
-            onClick={e => this.fetchInitialChannelAmount(e)}>
-            View Initial Channel Amount
+
+          <label>
+            Enter Channel Value
+            <input
+              type="text"
+              placeholder="amount in ETH"
+              value={openPaymentAmount}
+              onChange={e => this.setState({ openPaymentAmount: e.target.value })} />
+          </label>
+
+          <button onClick={e => this.openChannel(e)}>
+            Open Channel
           </button>
         </form>
 
+        <hr/>
+
+        <h1>Sender channels</h1>
         {
-          localStorage.getItem('contractAddress')
+          senderChannels.length > 0 &&
+          <div>
+            {
+              senderChannels.map(channel => (
+                <div>
+                  {channel.id} &nbsp;
+                  { web3.utils.fromWei(this.getPreviousChannelSpend(channel.id).toString()) } &nbsp; / &nbsp;
+                  { web3.utils.fromWei(channel.deposit.toString(), 'ether') } Eth &nbsp;
+                  {channel.recipient} &nbsp;
+                  <button onClick={() => this.initPayment(channel.id)}>
+                    make a payment { paymentAmountEth } ETH
+                  </button>
+                  <button
+                    onClick={() => this.verifySignature(channel.id)}
+                    disabled={this.getPreviousChannelSpend(channel.id) === 0}>
+                    verify signature
+                  </button>
+                </div>
+              ))
+            }
+          </div>
         }
-
-        <br/><br/>
-
-        Payment channel address
-        <h3>{ channelContractAddress }</h3>
-
-        <h1>Make a payment</h1>
-        <form>
-          <label>
-            Enter amount
-            <input
-              type="text"
-              onChange={e => this.setState({ paymentAmount: e.target.value })} />
-          </label>
-          <button
-            onClick={e => this.constructPaymentMessage(e)}>
-            Sign Payment
-          </button>
-        </form>
-
-        { signedMessage }
-
-
-        <br/><br/>
-
-        <h1>Verify Payment</h1>
-        <form>
-          <label>
-            Enter signed message
-            <input
-              type="text"
-              onChange={e => this.setState({ verifyMessage: e.target.value })} />
-          </label>
-          <button
-            onClick={e => this.verifyPayment(e)}>
-            Verify payment
-          </button>
-        </form>
+        {
+          !senderChannels.length &&
+          <div>
+            no channels
+          </div>
+        }
       </div>
     )
   }
+
 }
 
 export default App
